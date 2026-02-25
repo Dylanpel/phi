a295914b67389bbf166a805b03c17e50210a95a0
Arnaud Gaudin <arnaudgaudin86@gmail.com>
2026-02-25 11:37:52 +0100

    feat: create pageController and remove dashboard route

---
+++ src/Controllers/PagesController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Core\Abstract\Controller;
+
+class PagesController extends Controller
+{
+  /**
+   * Affiche la vue de la home
+   * @return void Pas de valeur de retour
+   */
+  public function home(): void
+  {
+    $this->render('pages/home', []);
+  }
+
+  /**
+   * Affiche la vue de la page d'erreur
+   * @return void Pas de valeur de retour
+   */
+  public function error(?array $data = []): void
+  {
+    $this->render('pages/error', $data);
+  }
+
+  /**
+   * Affiche la vue de la page d'accès interdit
+   * @return void Pas de valeur de retour
+   */
+  public function forbidden(?array $data = []): void
+  {
+    $this->render('pages/forbidden', $data);
+  }
+}
