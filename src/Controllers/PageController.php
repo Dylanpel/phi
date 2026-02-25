bd1f46c0436eb982beb7e2f80c252fa8475dce3d
Arnaud Gaudin <arnaudgaudin86@gmail.com>
2026-02-25 17:08:11 +0100

    refactor: rename PagesController to PageController

--- src/Controllers/PageController.php
+++ src/Controllers/PageController.php
@@ -4,7 +4,7 @@ namespace App\Controllers;
 
 use App\Core\Abstract\Controller;
 
-class PagesController extends Controller
+class PageController extends Controller
 {
   /**
    * Affiche la vue de la home
