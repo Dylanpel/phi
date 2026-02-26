e2ed72106aa7c9204d82b3bb635de92aa018197b
Arnaud Gaudin <arnaudgaudin86@gmail.com>
2026-02-26 15:55:36 +0100

    feat: handle navbar scroll and padding

--- public/js/script.js
+++ public/js/script.js
@@ -1,4 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
-  // Ton code ici
-  console.log('PHI - DOM chargé');
-});
+// ajustement du padding du body pour la navbar
+function navbarBodyPadding() {
+  const navbar = document.getElementById('mainNav');
+  const style = window.getComputedStyle(navbar);
+  const marginTop = parseFloat(style.marginTop);
+  const marginBottom = parseFloat(style.marginBottom);
+  document.body.style.paddingTop = (navbar.offsetHeight + marginTop + marginBottom) + 'px';
+}
+
+function navbarScroll() {
+  const navbar = document.getElementById('mainNav');
+  navbar.classList.toggle('scrolled', window.scrollY > 75);
+}
+
+document.addEventListener('DOMContentLoaded', navbarBodyPadding);
+window.addEventListener('resize', navbarBodyPadding);
+window.addEventListener('scroll', navbarScroll);
