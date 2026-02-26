c948651e862fa2aa4ef2a7e81fe02e5e68505286
Arnaud Gaudin <arnaudgaudin86@gmail.com>
2026-02-26 17:33:51 +0100

    feat: move connexion in userBubble

--- public/js/script.js
+++ public/js/script.js
@@ -12,6 +12,22 @@ function navbarScroll() {
   navbar.classList.toggle('scrolled', window.scrollY > 75);
 }
 
-document.addEventListener('DOMContentLoaded', navbarBodyPadding);
+function userBubbleInit() {
+  const el = document.querySelector('[data-bs-toggle="popover"]');
+  const popover = new bootstrap.Popover(el, { sanitize: false });
+
+  // Fermer le popover en cliquant ailleurs
+  document.addEventListener('click', function (e) {
+    if (!el.contains(e.target)) {
+      popover.hide();
+    }
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  navbarBodyPadding();
+  userBubbleInit();
+});
+
 window.addEventListener('resize', navbarBodyPadding);
 window.addEventListener('scroll', navbarScroll);
