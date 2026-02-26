741f035c8f473fe5846c95177f3a551abedc6a0f
Arnaud Gaudin <arnaudgaudin86@gmail.com>
2026-02-26 11:24:53 +0100

    feat: create articleController

--- src/Controllers/ArticleController.php
+++ src/Controllers/ArticleController.php
@@ -29,4 +29,99 @@ class ArticleController extends Controller
       'articles' => $this->articleManager->findAll()
     ]);
   }
+
+  /**
+   * Affiche la vue du formulaire de création d'article
+   * @return void Pas de valeur de retour
+   */
+  public function createForm(): void
+  {
+    //obligation de la connexion en tant qu'admin
+    $this->requireAdmin();
+
+    $this->render('admin/article/form', [
+      'action' => '/admin/article/form'
+    ]);
+  }
+
+  /**
+   * Traitement du formulaire de création d'article
+   * @return void Pas de valeur de retour
+   */
+  public function create(): void
+  {
+    //obligation de la connexion en tant qu'admin
+    $this->requireAdmin();
+
+    //récupération des infos envoyées par le formulaire
+    $title = trim($_POST['title'] ?? '');
+    $slug = trim($_POST['slug'] ?? '');
+    $date = trim($_POST['date'] ?? '');
+    $content = trim($_POST['content'] ?? '');
+    $imageUrl = trim($_POST['image_url'] ?? '');
+
+    $data = [
+      'title' => $title,
+      'slug' => $slug,
+      'date' => $date,
+      'content' => $content,
+    ];
+    
+    if(!empty($imageUrl)) {
+      $data['image_url'] = $imageUrl;
+    }
+    
+    //création de l'article en base et récupération entité de ce dernier
+    $article = $this->articleManager->create($data);
+    
+    //redirection vers le formulaire de modification
+    $id = $article->getId();
+    Router::redirect("/admin/article/$id/form");
+  }
+
+  /**
+   * Affiche la vue du formulaire de modification de page
+   * @return void Pas de valeur de retour
+   */
+  public function updateForm(int $id): void
+  {
+    //obligation de la connexion en tant qu'admin
+    $this->requireAdmin();
+
+    $this->render('admin/article/form', [
+      'action' => "/admin/article/$id/form",
+      'page' => $this->articleManager->findById($id)
+    ]);
+  }
+
+  /**
+   * Traitement du formulaire de modification de page
+   * @return void Pas de valeur de retour
+   */
+  public function update(int $id): void
+  {
+    //obligation de la connexion en tant qu'admin
+    $this->requireAdmin();
+    
+    //récupération des infos envoyées par le formulaire
+    $title = trim($_POST['title'] ?? '');
+    $slug = trim($_POST['slug'] ?? '');
+    $date = trim($_POST['date'] ?? '');
+    $content = trim($_POST['content'] ?? '');
+    $imageUrl = trim($_POST['image_url'] ?? '');
+
+    //création object contenant les données à mettre à jour
+    $data = [
+      'title' => $title,
+      'slug' => $slug,
+      'date' => $date,
+      'content' => $content,
+      'image_url' => $imageUrl,
+    ];
+
+    $this->articleManager->update($id, $data);
+
+    //redirection vers le formulaire de modification
+    Router::redirect("/admin/article/$id/form");
+  }
 }
